df3cd1a9f16eb9a700ecadb75edfb537a3f0fc96
Sailwayfun <liaoleon000513@gmail.com>
2024-02-29 21:47:47 +0800

    write state and effect logic

--- src/components/TrafficLight.tsx
+++ src/components/TrafficLight.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+
 const TrafficLight = () => {
-    return <div>Traffic Light</div>
-}
+  const [currentLight, setCurrentLight] = useState<number>(0);
+
+  useEffect(() => {
+    function getTimeout(): number {
+      if (currentLight === 0) {
+        return 4000;
+      } else if (currentLight === 1) {
+        return 500;
+      } else {
+        return 3000;
+      }
+    }
+
+    function switchLights() {
+      return currentLight < 2
+        ? setCurrentLight((prev) => prev + 1)
+        : setCurrentLight(0);
+    }
+
+    const timeout = setTimeout(switchLights, getTimeout());
+
+    return () => clearTimeout(timeout);
+  }, [currentLight]);
+
+  return (
+    <div>
+      <div>Traffic Light</div>
+      <div>{currentLight}</div>
+    </div>
+  );
+};
 
 export default TrafficLight;
